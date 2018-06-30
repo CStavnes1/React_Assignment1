cfac6abddb6117516595dd2033739e57777ecac0
ChrisStavnes <ChrisStavnes@gmail.com>
2018-06-30 13:24:58 -0500

    deploy to ghpages

--- src/App.js
+++ src/App.js
@@ -20,32 +20,29 @@ class App extends Component {
   
   endGame = () => {
     if (this.state.score > this.state.highscore) {
-      this.setState({ highscore: this.state.score});
+      this.setState({ highscore: this.state.score });
     }
-   
+
   }
 
   handleClick = (id) => {
     console.log(this.arr)
-     if (this.state.arr.includes(id)) {
+    if (this.state.arr.includes(id)) {
       this.endGame();
-       (alert("Game Over"))
-       this.setState({ score: 0});
-     }
-     else {
-    
-    this.setState((prevState, arr) => ({
-      score: prevState.score + 1,
-      test: this.state.arr.push(id)
-    }));
-    this.state.teams.sort(() => Math.random() - 0.5)
-    return true;
-  }
+      (alert("Game Over"))
+      this.setState({ score: 0 });
+    }
+    else {
+
+      this.setState((prevState, arr) => ({
+        score: prevState.score + 1,
+        test: this.state.arr.push(id)
+      }));
+      this.state.teams.sort(() => Math.random() - 0.5)
+      return true;
+    }
 
 
-    // function addToArray(count){
-    //   id.push(count)
-    // }
     console.log(this.state.score)
     console.log(id)
 
