8a63ea8a7ef258d064d61b36a2bcf46c94c326ec
ChrisStavnes <ChrisStavnes@gmail.com>
2018-06-30 12:57:29 -0500

    finished hw

--- src/App.js
+++ src/App.js
@@ -20,22 +20,27 @@ class App extends Component {
   
   endGame = () => {
     if (this.state.score > this.state.highscore) {
-      this.setState({ highscore: this.state.score }, function () {
-        // console.log(this.state.highscore)
-      });
+      this.setState({ highscore: this.state.score});
     }
-
+   
   }
 
   handleClick = (id) => {
-
+    console.log(this.arr)
+     if (this.state.arr.includes(id)) {
+      this.endGame();
+       (alert("Game Over"))
+       this.setState({ score: 0});
+     }
+     else {
+    
     this.setState((prevState, arr) => ({
       score: prevState.score + 1,
       test: this.state.arr.push(id)
     }));
     this.state.teams.sort(() => Math.random() - 0.5)
     return true;
-
+  }
 
 
     // function addToArray(count){
@@ -43,7 +48,7 @@ class App extends Component {
     // }
     console.log(this.state.score)
     console.log(id)
-    this.endGame()
+
   } 
 
 
@@ -54,7 +59,7 @@ class App extends Component {
       <Wrapper>
         <Title>Teams</Title>
         <div>Score:{this.state.score}</div>
-        <div>High Score:{this.state.score}</div>
+        <div>High Score:{this.state.highscore}</div>
         <br/>
         {this.state.teams.map(team => (
           <TeamCard
